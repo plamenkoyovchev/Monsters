b63d456df961203801dad8db1978517e5423eaf2
plamenyovchev <plamenkooo@gmail.com>
2019-11-10 16:58:34 +0200

    Fetch monsters & pass them to CardList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import CardList from './components/Cards/CardList/CardList';
+
 function App() {
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(monsters => setMonsters(monsters));
+  }, []);
+
   return (
     <div className="App">
-
+      <CardList monsters={monsters} />
     </div>
   );
 }
