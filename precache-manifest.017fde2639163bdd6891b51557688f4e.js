bec3b43b90895267af2ab812cce228cf9d51b4aa
plamenyovchev <plamenkooo@gmail.com>
2019-11-10 20:40:11 +0200

    Updates

---
+++ precache-manifest.017fde2639163bdd6891b51557688f4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d50e6e74b66dc59770ad4829e10670d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "fff2375616a2870aa9e6",
+    "url": "/monsters/static/css/main.6313f31a.chunk.css"
+  },
+  {
+    "revision": "9c97ca96bff69a7e1537",
+    "url": "/monsters/static/js/2.ffa2392c.chunk.js"
+  },
+  {
+    "revision": "fff2375616a2870aa9e6",
+    "url": "/monsters/static/js/main.ff2d3f08.chunk.js"
+  },
+  {
+    "revision": "e038688c1a818053149e",
+    "url": "/monsters/static/js/runtime-main.50077317.js"
+  }
+]);
