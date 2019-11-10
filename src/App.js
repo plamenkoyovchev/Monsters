1088e33b6c2f1bff6634cb6464065f232cee01f6
plamenyovchev <plamenkooo@gmail.com>
2019-11-10 17:34:21 +0200

    Add SearchBox component to App component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import CardList from './components/Cards/CardList/CardList';
+import SearchBox from './components/UI/SearchBox/SearchBox';
 
 function App() {
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
 
   useEffect(() => {
     fetch('http://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(monsters => setMonsters(monsters));
+      .then(monsters => {
+        setMonsters(monsters);
+        setFilteredMonsters(monsters);
+      });
   }, []);
+
+  const onSearchChangeHandler = (event) => {
+    setFilteredMonsters(monsters.filter(monster =>
+      monster.name.toLowerCase().includes(event.target.value.toLowerCase())
+    ));
+  };
 
   return (
     <div className="App">
-      <CardList monsters={monsters} />
+      <SearchBox changed={onSearchChangeHandler} placeholder='Search' />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
