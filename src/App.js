2320a7ad21870d620f8c17225ca8ef74cf280a4c
plamenyovchev <plamenkooo@gmail.com>
2019-11-10 20:45:01 +0200

    Use secured resource for the images

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   );
 
   useEffect(() => {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(monsters => setMonsters(monsters));
   }, []);
